aef6ad0c20c7c0a8f6013bcda70d5b73e791d6c6
Redtrain22 <Redtrain22@users.noreply.github.com>
2023-07-07 19:20:55 -0400

    chore(formatting)

--- RoR2TierSelector/Network.cs
+++ RoR2TierSelector/Network.cs
@@ -15,7 +15,7 @@ namespace RoR2TierSelector
 		private List<ConfigEntry<int>> items = new List<ConfigEntry<int>>();
 		private List<ConfigEntry<int>> equipments = new List<ConfigEntry<int>>();
 
-		public SyncConfig() {}
+		public SyncConfig() { }
 
 		public SyncConfig(NetworkInstanceId netId, List<ConfigEntry<int>> items, List<ConfigEntry<int>> equipments)
 		{
@@ -26,56 +26,56 @@ namespace RoR2TierSelector
 
 		public void Deserialize(NetworkReader reader)
 		{
-        // Read the byte array from the NetworkReader
-        byte[] data = reader.ReadBytesAndSize();
+			// Read the byte array from the NetworkReader
+			byte[] data = reader.ReadBytesAndSize();
 
-        // Create a MemoryStream from the byte array
-        using (MemoryStream stream = new MemoryStream(data))
-        {
-            // Deserialize the configuration data from the MemoryStream
-            XmlSerializer serializer = new XmlSerializer(typeof(List<ConfigEntry<int>>));
-        		netId = (NetworkInstanceId)serializer.Deserialize(stream);
-        		items = (List<ConfigEntry<int>>)serializer.Deserialize(stream);
-        		equipments = (List<ConfigEntry<int>>)serializer.Deserialize(stream);
-        }
+			// Create a MemoryStream from the byte array
+			using (MemoryStream stream = new MemoryStream(data))
+			{
+				// Deserialize the configuration data from the MemoryStream
+				XmlSerializer serializer = new XmlSerializer(typeof(List<ConfigEntry<int>>));
+				netId = (NetworkInstanceId)serializer.Deserialize(stream);
+				items = (List<ConfigEntry<int>>)serializer.Deserialize(stream);
+				equipments = (List<ConfigEntry<int>>)serializer.Deserialize(stream);
+			}
 		}
 
-    public void OnReceived()
-    {
-      if (NetworkServer.active)
-      {
-        RoR2TierSelector.tierLogger.Log("SyncConfig: Host ran this. Skip.");
-				
-        return;
-      }
+		public void OnReceived()
+		{
+			if (NetworkServer.active)
+			{
+				RoR2TierSelector.tierLogger.Log("SyncConfig: Host ran this. Skip.");
+
+				return;
+			}
 			Chat.AddMessage($"Client received SyncConfig.");
 
-      // Update the items and equipments lists in the ConfigManager with the received lists
-      ConfigManager.items = this.items;
-      ConfigManager.equipments = this.equipments;
+			// Update the items and equipments lists in the ConfigManager with the received lists
+			ConfigManager.items = this.items;
+			ConfigManager.equipments = this.equipments;
 			// RoR2TierSelector.ReloadItemTiers();
-    }
+		}
 
 
 
-    public void Serialize(NetworkWriter writer)
-    {
-      // Create a MemoryStream to hold the serialized data
-      using (MemoryStream stream = new MemoryStream())
-      {
-        // Serialize the configuration data to XML and write it to the MemoryStream
-        XmlSerializer serializer = new XmlSerializer(typeof(List<ConfigEntry<int>>));
+		public void Serialize(NetworkWriter writer)
+		{
+			// Create a MemoryStream to hold the serialized data
+			using (MemoryStream stream = new MemoryStream())
+			{
+				// Serialize the configuration data to XML and write it to the MemoryStream
+				XmlSerializer serializer = new XmlSerializer(typeof(List<ConfigEntry<int>>));
 				serializer.Serialize(stream, netId);
-        serializer.Serialize(stream, items);
+				serializer.Serialize(stream, items);
 				serializer.Serialize(stream, equipments);
 
-        // Get the byte array representation of the serialized data
-        byte[] data = stream.ToArray();
-				
-        // Write the byte array to the NetworkWriter
-        writer.WriteBytesAndSize(data, data.Length);
-      }
-    }
+				// Get the byte array representation of the serialized data
+				byte[] data = stream.ToArray();
+
+				// Write the byte array to the NetworkWriter
+				writer.WriteBytesAndSize(data, data.Length);
+			}
+		}
 
 	}
 }
--- RoR2TierSelector/RoR2TierSelector.cs
+++ RoR2TierSelector/RoR2TierSelector.cs
@@ -11,7 +11,7 @@ using ItemCatalog = On.RoR2.ItemCatalog;
 using EquipmentCatalog = On.RoR2.EquipmentCatalog;
 
 // New way to register commands
-[assembly: HG.Reflection.SearchableAttribute.OptInAttribute]
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
 namespace RoR2TierSelector
 {
 	// Dependancies
@@ -59,13 +59,13 @@ namespace RoR2TierSelector
 		{
 			Logger.Log(LogLevel.Info, $"Loaded {PluginName} v{PluginVersion}");
 			config = new ConfigManager();
-	
+
 			NetworkingAPI.RegisterMessageType<SyncConfig>();
 
 			// Hooks
 			EquipmentCatalog.RegisterEquipment += RegisterEquipmentHook;
 			ItemCatalog.SetItemDefs += SetItemDefsHook;
-			Logger.Log(LogLevel.Info,$"Hooks Added.");
+			Logger.Log(LogLevel.Info, $"Hooks Added.");
 		}
 		private void checkGameVersion(On.RoR2.RoR2Application.orig_Awake orig, RoR2Application self)
 		{
@@ -78,45 +78,45 @@ namespace RoR2TierSelector
 			orig(self);
 		}
 
-    private static void ReloadItemTiers(ItemIndex[] itemIndexes)
-    {
-      // // Using "Reflection" to grab the itemDefs Directly out of scope
-      FieldInfo itemDefsField = typeof(ItemCatalog).GetField("itemDefs", BindingFlags.NonPublic | BindingFlags.Static);
-      // itemDefsField is static so reflecting it doesnt require an instance
-      ItemDef[] itemDefs = (ItemDef[])itemDefsField.GetValue(null);
-      foreach (var itemIndex in itemIndexes)
-      {
-        ItemDef itemDef = itemDefs[(int)itemIndex];
-        if (itemDef != null)
-        {
-          int index = ConfigManager.items.FindIndex(configItem => (string)configItem.Definition.Key == itemDef.name);
-          ItemTier tier = (ItemTier)ConfigManager.items.ElementAt(index).Value;
-          if (itemDef.tier != tier)
-          {
-            itemDef.tier = tier;
-          }
-        }
-      }
-      // Getting the SetItemDefs method
-      MethodInfo setItemDefsMethod = typeof(ItemCatalog).GetMethod("Init", BindingFlags.NonPublic | BindingFlags.Static);
+		private static void ReloadItemTiers(ItemIndex[] itemIndexes)
+		{
+			// // Using "Reflection" to grab the itemDefs Directly out of scope
+			FieldInfo itemDefsField = typeof(ItemCatalog).GetField("itemDefs", BindingFlags.NonPublic | BindingFlags.Static);
+			// itemDefsField is static so reflecting it doesnt require an instance
+			ItemDef[] itemDefs = (ItemDef[])itemDefsField.GetValue(null);
+			foreach (var itemIndex in itemIndexes)
+			{
+				ItemDef itemDef = itemDefs[(int)itemIndex];
+				if (itemDef != null)
+				{
+					int index = ConfigManager.items.FindIndex(configItem => (string)configItem.Definition.Key == itemDef.name);
+					ItemTier tier = (ItemTier)ConfigManager.items.ElementAt(index).Value;
+					if (itemDef.tier != tier)
+					{
+						itemDef.tier = tier;
+					}
+				}
+			}
+			// Getting the SetItemDefs method
+			MethodInfo setItemDefsMethod = typeof(ItemCatalog).GetMethod("Init", BindingFlags.NonPublic | BindingFlags.Static);
 
-      // Invoke the SetItemDefs method passing the new itemDefs
-      setItemDefsMethod.Invoke(null, new object[] { itemDefs });
-    }
+			// Invoke the SetItemDefs method passing the new itemDefs
+			setItemDefsMethod.Invoke(null, new object[] { itemDefs });
+		}
 		public static void ReloadItemTiers()
-    {
-      ItemIndex[] itemIndexes = RoR2.ItemCatalog.allItems.ToArray();
-      ReloadItemTiers(itemIndexes);
+		{
+			ItemIndex[] itemIndexes = RoR2.ItemCatalog.allItems.ToArray();
+			ReloadItemTiers(itemIndexes);
 
 			// // Getting the SetItemDefs method
-      //   MethodInfo setInitMethod = typeof(ItemCatalog).GetMethod("Init", BindingFlags.NonPublic | BindingFlags.Static);
-        
-      //   // Invoke the SetItemDefs method passing the new itemDefs
+			//   MethodInfo setInitMethod = typeof(ItemCatalog).GetMethod("Init", BindingFlags.NonPublic | BindingFlags.Static);
+
+			//   // Invoke the SetItemDefs method passing the new itemDefs
 			// 	setInitMethod.Invoke(null, null);
 
-      // Reload item GUI settings if needed
-      config.AddItemGUISettings();
-    }
+			// Reload item GUI settings if needed
+			config.AddItemGUISettings();
+		}
 		private void SetItemDefsHook(ItemCatalog.orig_SetItemDefs orig, ItemDef[] itemDefs)
 		{
 			foreach (ItemDef item in itemDefs)
@@ -133,7 +133,7 @@ namespace RoR2TierSelector
 			config.AddItemGUISettings();
 			orig.Invoke(itemDefs);
 		}
- 
+
 		private void RegisterEquipmentHook(EquipmentCatalog.orig_RegisterEquipment orig, EquipmentIndex equipmentIndex, EquipmentDef equipDef)
 		{
 			config.AddEquipmentToList(ConfigManager.equipments, equipDef);
